3e39c5a88686b0583e8c75a7cb89b6a780b88445
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2021-01-15 10:12:02 -0300

    ensure HttpAdapter returns null if post returns 200 without data

--- hunter_flutter/test/infra/http/http_adapter_test.dart
+++ hunter_flutter/test/infra/http/http_adapter_test.dart
@@ -22,7 +22,7 @@ class HttpAdapter implements HttpClient {
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -69,5 +69,13 @@ void main() {
     final response = await sut.request(url: url, method: 'post');
 
     expect(response, {'any_key': 'any_value'});
+  });
+  test('Should return null if post returns 200 without data', () async {
+    when(client.post(any, headers: anyNamed('headers')))
+        .thenAnswer((_) async => Response('', 200));
+
+    final response = await sut.request(url: url, method: 'post');
+
+    expect(response, null);
   });
 }
