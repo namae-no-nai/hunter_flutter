24af3841923944cc05b1da232f4fa78ea3a1b0cf
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2021-01-15 08:41:30 -0300

    ensure HttpAdapter calls post with correct Body

--- hunter_flutter/test/infra/http/http_adapter_test.dart
+++ hunter_flutter/test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -9,12 +11,14 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({@required String url, @required String method}) async {
+  Future<void> request(
+      {@required String url, @required String method, Map body}) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(url, headers: headers);
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -33,12 +37,20 @@ void main() {
 
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-      verify(client.post(url, headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-      }));
+      verify(client.post(url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: '{"any_key":"any_value"}'));
     });
+  });
+  test('Should call post without body', () async {
+    await sut.request(url: url, method: 'post');
+
+    verify(client.post(any, headers: anyNamed('headers')));
   });
 }
