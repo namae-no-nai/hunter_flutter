2b64cb90210117ac374ea6ab936331b2771d3e6f
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2021-01-22 09:47:05 -0300

    move authetication to a helper method

--- hunter_flutter/test/presentation/presenters/stream_login_presenter_test.dart
+++ hunter_flutter/test/presentation/presenters/stream_login_presenter_test.dart
@@ -37,10 +37,19 @@ void main() {
   ValidationSpy validation;
   String email;
 
+  PostExpectation mockValidationCall(String field) => when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed('value')));
+
+  void mockValidation({String field, String value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
+
   setUp(() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    mockValidation();
   });
 
   test('Should call Validation with correct email', () {
@@ -50,10 +59,6 @@ void main() {
   });
 
   test('Should emit email error if validation fails', () {
-    when(validation.validate(
-            field: anyNamed('field'), value: anyNamed('value')))
-        .thenReturn('error');
-
     expectLater(sut.emailErrorStream, emits('error'));
 
     sut.validateEmail(email);
